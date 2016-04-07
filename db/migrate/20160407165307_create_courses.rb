cb32f61f3797be04b1a922bca0a0834f5a47c273
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-07 15:26:57 -0700

    courses: change credits to float
    * Allow for half credit courses

--- db/migrate/20160407165307_create_courses.rb
+++ db/migrate/20160407165307_create_courses.rb
@@ -3,7 +3,7 @@ class CreateCourses < ActiveRecord::Migration
     create_table :courses do |t|
       t.text :title
       t.text :instructor
-      t.integer :credits
+      t.float :credits
       t.text :description
       t.text :requirements
       t.text :term
