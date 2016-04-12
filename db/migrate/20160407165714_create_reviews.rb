e0da2b03b81448af89967f2a474d8971c78d9a5c
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-12 07:48:58 -0700

    reviews: add course code parameter

--- db/migrate/20160407165714_create_reviews.rb
+++ db/migrate/20160407165714_create_reviews.rb
@@ -6,6 +6,7 @@ class CreateReviews < ActiveRecord::Migration
       t.text :date
       t.text :info
       t.text :review_text
+      t.text :course_code
 
       t.timestamps null: false
     end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -18,7 +18,7 @@ class ReviewsControllerTest < ActionController::TestCase
 
   test "should create review" do
     assert_difference('Review.count') do
-      post :create, review: { date: @review.date, info: @review.info, rank: @review.rank, rating: @review.rating, review_text: @review.review_text }
+      post :create, review: { date: @review.date, info: @review.info, rank: @review.rank, rating: @review.rating, review_text: @review.review_text, course_code: @review.course_code }
     end
 
     assert_redirected_to review_path(assigns(:review))
@@ -35,7 +35,7 @@ class ReviewsControllerTest < ActionController::TestCase
   end
 
   test "should update review" do
-    patch :update, id: @review, review: { date: @review.date, info: @review.info, rank: @review.rank, rating: @review.rating, review_text: @review.review_text }
+    patch :update, id: @review, review: { date: @review.date, info: @review.info, rank: @review.rank, rating: @review.rating, review_text: @review.review_text, course_code: @review.course_code }
     assert_redirected_to review_path(assigns(:review))
   end
 
