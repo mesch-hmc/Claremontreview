bc9929e1f2f8e9157188dc1fd367b1175ea507e0
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-16 23:38:32 -0700

    review: data validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :course
+
+  validates :rating, presence: true
+  validates :rating, :inclusion => 0..5
+  validates :info, presence: true
+  validates :review_text, presence: true
 end
