67dec11408fef38f3675fa9e2d3747e08a6aa3c5
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-18 06:41:24 -0700

    authenticate users before adding review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+    before_action :authenticate_user!, only: [:create]
     
     def create
         @course = Course.find_by_code(params[:course_code])
