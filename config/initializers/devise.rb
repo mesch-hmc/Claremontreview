25792b9a47319dcc4a4ebc0b1bd3ec7448ed2979
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-18 07:19:56 -0700

    Implement Google Sign-In

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,6 +239,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV[' GOOGLE_CLIENT_SECRET'],
+    :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :courses, param: :code do
     resources :reviews
   end
