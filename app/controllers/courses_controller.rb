1d6fc7991072564f2851782fd2710101b6b902f9
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-17 21:22:03 -0700

    courses: prevent creating new courses
    * we got it all. Thanks aspc!

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -19,7 +19,8 @@ class CoursesController < ApplicationController
 
   # GET /courses/new
   def new
-    @course = Course.new
+    #@course = Course.new
+    redirect_to courses_url
   end
 
   # GET /courses/1/edit
