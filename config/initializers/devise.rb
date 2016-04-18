5e621de43890f80d285f51a8019feb9ec5a22880
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-18 07:48:01 -0700

    fix ssl issues running on local

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,8 +239,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   config.omniauth :google_oauth2, "461789162933-mjedfk153l4ff4h0babauifh85ej6j6u.apps.googleusercontent.com",
-      "ZYY60_uUIwWvVphwh61_wli_", :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
+      "ZYY60_uUIwWvVphwh61_wli_"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
