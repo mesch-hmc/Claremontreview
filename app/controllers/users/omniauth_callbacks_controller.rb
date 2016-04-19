d37f16d31585d4b9d47ad67d61a0fc5cc2b4af3b
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-18 17:06:21 -0700

    add flash messages

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user = User.from_omniauth(request.env["omniauth.auth"])
 
         if @user.persisted?
-            flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
             if (@user.email =~ /.+@.*.edu/)
+                flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
                 sign_in_and_redirect @user, :event => :authentication
             else
+                flash[:warning] = "Failure: edu email required"
                 redirect_to root_url
             end
         else
