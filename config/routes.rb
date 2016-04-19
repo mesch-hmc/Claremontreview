6a7c50f323cc1bf213904efa27a7e4ce9342baf0
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-18 20:54:14 -0700

    courses: prevent from edit or destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :courses, param: :code do
+  resources :courses, except: [:edit, :destroy], param: :code do
     resources :reviews
   end
   # The priority is based upon order of creation: first created -> highest priority.
