8e56ddca0d3ebe52ccb62c7b281bf1aa853308a2
edo32 <ehimen97@gmail.com>
2016-04-20 19:59:44 +0000

    grades dropdown

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,10 @@ class Review < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
 
-  validates :rating, presence: true, :inclusion => 0..5
-  validates :grade, presence: true
+  GRADES = ['A+','A','A-','B+','B','B-','C+','C','C-','D+','D','D-','F','PASS','FAIL','NC']
+
+  validates :rating, presence: true, inclusion: 0..5
+  validates :grade, presence: true, inclusion: {in: GRADES}
   validates :info, presence: true
   validates :review_text, presence: true
 
