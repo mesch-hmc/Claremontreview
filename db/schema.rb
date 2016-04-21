5e33e1db74206feb50d2dcea61667cfea54412f4
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-20 21:20:46 -0700

    user: add admin attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419035709) do
+ActiveRecord::Schema.define(version: 20160421041644) do
 
   create_table "courses", force: :cascade do |t|
     t.text     "code"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160419035709) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
