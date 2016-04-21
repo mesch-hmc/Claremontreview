1ed6acca546a2c43760db80f98836cb586ee49ca
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-04-20 20:56:48 -0700

    review: update grades
    * change PASS to P
    * change FAIL to F
    * add W for withdraw

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
 
-  GRADES = ['A+','A','A-','B+','B','B-','C+','C','C-','D+','D','D-','F','PASS','FAIL','NC']
+  GRADES = ['A+','A','A-','B+','B','B-','C+','C','C-','D+','D','D-','F','P','F','W','NC']
 
   validates :rating, presence: true, inclusion: 0..5
   validates :grade, presence: true, inclusion: {in: GRADES}
