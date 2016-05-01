509a43b53b19bd1592dc484e6ce440d1afa9d8ce
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-05-01 15:02:05 -0700

    js: gradechart: use schoolColor function

--- app/assets/javascripts/colors.js
+++ app/assets/javascripts/colors.js
@@ -21,6 +21,15 @@ function schoolColor() {
     }
     return color;
 };
+function convertHex(hex,opacity){
+    hex = hex.replace('#','');
+    r = parseInt(hex.substring(0,2), 16);
+    g = parseInt(hex.substring(2,4), 16);
+    b = parseInt(hex.substring(4,6), 16);
+
+    result = 'rgba('+r+','+g+','+b+','+opacity/100+')';
+    return result;
+}
 
 var ready;
 ready = function() {
--- app/assets/javascripts/gradechart.js
+++ app/assets/javascripts/gradechart.js
@@ -1,32 +1,17 @@
 var ready;
 ready = function() {
-    if (window.location.href.split('/')[3] != null && window.location.href.split('/')[3] == 'courses') {
+    if (coursePage()) {
         grade_distr = $("#gradeDistribution").html().split(","), a = new Array, labels = new Array;
         for (var e = 0; e < grade_distr.length; e++) 0 != grade_distr[e].length && (datapts = grade_distr[e].split(":"), labels.push(datapts[0]), a.push(parseInt(datapts[1])));
         new_labels = ["A+","A","A-","B+","B","B-","C+","C","C-","D+","D","D-","F","HP","P","NC"], new_data = new Array(new_labels.length);
         for (var e = 0; e < labels.length; e++) {
             var t = labels.indexOf(new_labels[e]); - 1 != t && (new_data[e] = a[t])
         }
-        course = window.location.href.split('/').pop();
-        school = course.substring(course.length-2, course.length);
-        if (school == 'CM') {
-            color = "rgba(152,27,50,0.5)";
-        } else if (school == 'HM') {
-            color = "rgba(234,170,0,0.5)";
-        } else if (school == 'PO') {
-            color = "rgba(32,67,143,0.5)";
-        } else if (school == 'PZ') {
-            color = "rgba(247,148,29,0.5)";
-        } else if (school == 'SC') {
-            color = "rgba(46,77,65,0.5)";
-        } else {
-            color = "rgba(234,170,0,0.5)";
-        }
         var a = {
             labels: new_labels,
             datasets: [{
-                backgroundColor: color,
-                borderColor: color,
+                backgroundColor: convertHex(schoolColor(), 100),
+                borderColor: convertHex(schoolColor(), 100),
                 data: new_data
             }]
         },
