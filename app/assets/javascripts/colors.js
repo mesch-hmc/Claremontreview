19e98da2534540813412cbcbc8364d917210fef6
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-05-01 14:58:29 -0700

    js: colors: export functions for reusability

--- app/assets/javascripts/colors.js
+++ app/assets/javascripts/colors.js
@@ -1,6 +1,10 @@
-var ready;
-ready = function() {
-    if (window.location.href.split('/')[3] != null && window.location.href.split('/')[3] == 'courses') {
+function coursePage() {
+    return (window.location.href.split('/')[3] != null
+        && window.location.href.split('/')[3] == 'courses');
+};
+function schoolColor() {
+    var color = "#EAAA00";
+    if (coursePage) {
         course = window.location.href.split('/').pop();
         school = course.substring(course.length-2, course.length);
         if (school == 'CM') {
@@ -13,18 +17,23 @@ ready = function() {
             color = "#f7941d";
         } else if (school == 'SC') {
             color = "#2e4d41";
-        } else {
-            color = "#EAAA00";
         }
+    }
+    return color;
+};
+
+var ready;
+ready = function() {
+    if (coursePage()) {
         var elements = document.getElementsByClassName('fa');
         for (var i=0; i<elements.length; i++) {
             var curr_element = elements[i];
-            curr_element.style.color=color;
+            curr_element.style.color=schoolColor();
         }
         var elements = document.getElementsByClassName('claremont');
         for (var i=0; i<elements.length; i++) {
             var curr_element = elements[i];
-            curr_element.style.color=color;
+            curr_element.style.color=schoolColor();
         }
     }
 };
