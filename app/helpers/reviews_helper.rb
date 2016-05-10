c72e57a8b60687f013b5929edd349a661fbd7db5
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-05-10 07:09:09 -0700

    reviews: anonymous college student

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,4 +2,22 @@ module ReviewsHelper
   def nl2br(s)
     s.gsub(/\n/, '<br>')
   end
+
+  def anon_student(email)
+    base = "by anonymous"
+    school = email.split("@")[1].split(".")[-2]
+    if school == "claremontmckenna"
+      base + " CMC student"
+    elsif school == "hmc"
+      base + " Mudd student"
+    elsif school == "pomona"
+      base + " Pomona student"
+    elsif school == "pitzer"
+      base + " Pitzer student"
+    elsif school == "scrippscollege"
+      base + " Scripps student"
+    else
+      base
+    end
+  end
 end
