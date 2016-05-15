82f7c02fce661bdca57a5dd2ed3290a9765f0dff
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-05-14 22:56:19 -0700

    controllers: application: fix redirect to current page after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    session[:previous_url] || root_path
+    request.referrer || root_path
   end
 end
