92a8b57b305b4f92a4a1b46f11c348b51b28f5d8
Jonathan Cruz <jcruzdeveloper@gmail.com>
2016-07-10 23:13:09 -0700

    [SQUASH] Materialize

--- app/assets/javascripts/clicksearch.js
+++ app/assets/javascripts/clicksearch.js
@@ -12,6 +12,12 @@ ready = function () {
     var course = data.item.code.replace(re, '_')
     window.location.href = '/courses/' + course
   })
+  $('#nav-search').bind('railsAutocomplete.select', function (event, data) {
+    var space = ' '
+    var re = new RegExp(space, 'g')
+    var course = data.item.code.replace(re, '_')
+    window.location.href = '/courses/' + course
+  })
   $('input[data-autocomplete][autofocus]').focus()
 }
 $(document).ready(ready)
--- app/assets/javascripts/colors.js
+++ app/assets/javascripts/colors.js
@@ -3,21 +3,60 @@ function coursePage () {
     && window.location.href.split('/')[3] === 'courses')
 }
 
-function schoolColor () {
-  var color = '#EAAA00'
-  if (coursePage) {
-    course = window.location.href.split('/').pop()
+function schoolInitials () {
+  school = 'HM'
+  if (coursePage()) {
+    course = window.location.href.split('/').pop().split('#')[0]
     school = course.substring(course.length-2, course.length)
+    return school
+  }
+  return school
+}
+
+function setSchoolColor (item) {
+  var school = schoolInitials()
+  if (school === 'CM') {
+    item.removeClass('yellow darken-3').addClass('pink darken-4')
+  } else if (school === 'PO') {
+    item.removeClass('yellow darken-3').addClass('blue darken-4')
+  } else if (school === 'PZ') {
+    item.removeClass('yellow darken-3').addClass('orange')
+  } else if (school === 'SC') {
+    item.removeClass('yellow darken-3').addClass('green darken-4')
+  }
+}
+
+function setSchoolTextColor (item) {
+  var school = schoolInitials()
+  if (school === 'CM') {
+    item.addClass('pink-text text-darken-4')
+  } else if (school === 'HM') {
+    item.addClass('yellow-text text-darken-3')
+  } else if (school === 'PO') {
+    item.addClass('blue-text text-darken-4')
+  } else if (school === 'PZ') {
+    item.addClass('orange-text')
+  } else if (school === 'SC') {
+    item.addClass('green-text text-darken-4')
+  } else {
+    item.addClass('yellow-text text-darken-3')
+  }
+}
+
+function schoolColor () {
+  var color = '#f9a825'
+  if (coursePage) {
+    school = schoolInitials()
     if (school === 'CM') {
-      color = '#981B32'
+      color = '#880e4f'
     } else if (school === 'HM') {
-      color = '#EAAA00'
+      color = '#f9a825'
     } else if (school === 'PO') {
-      color = '#20438f'
+      color = '#0d47a1'
     } else if (school === 'PZ') {
-      color = '#f7941d'
+      color = '#ff9800'
     } else if (school === 'SC') {
-      color = '#2e4d41'
+      color = '#1b5e20'
     }
   }
   return color
@@ -35,18 +74,21 @@ function convertHex (hex, opacity)  {
 
 var ready
 ready = function () {
-  if (coursePage()) {
-    var elements = document.getElementsByClassName('school-color')
-    for (var i = 0; i < elements.length; i++) {
-      var curr_element = elements[i]
-      curr_element.style.color=schoolColor()
-    }
-    var elements = document.getElementsByClassName('claremont')
-    for (var i = 0; i < elements.length; i++) {
-      var curr_element = elements[i]
-      curr_element.style.color = schoolColor()
-    }
-  }
+  setSchoolColor($('nav'))
+  $('.school-color').each(function () {
+    setSchoolTextColor($(this))
+  })
+  $('ul li span').each(function () {
+    setSchoolTextColor($(this))
+  })
+  $('<style>.col input:focus { border-bottom: 1px solid ' + schoolColor() + ' !important; box-shadow: 0 1px 0 0' + schoolColor() + ' !important; }</style>').appendTo('head')
+  $('<style>.col textarea:focus { border-bottom: 1px solid ' + schoolColor() + ' !important; box-shadow: 0 1px 0 0' + schoolColor() + ' !important; }</style>').appendTo('head')
 }
 $(document).ready(ready)
 $(document).on('page:load', ready)
+
+function updateSchoolColors () {
+  $('.school-color').each(function () {
+    $(this).css('color', schoolColor())
+  })
+}
