4e1e4c467f0e8228fc686a2f41f5e2e1b1623847
edo32 <ehimen97@gmail.com>
2016-12-31 11:10:44 -0800

    My reviews: fix authentication bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  before_action :authenticate_user!, only: [:my_reviews]
+  before_action :authenticate_user!, only: [:user_reviews]
 
   def index
   end
