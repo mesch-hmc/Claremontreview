ba23c3430e41567bebefcdc2970da6b7057d19f7
edo32 <ehimen97@gmail.com>
2016-12-31 18:27:06 -0800

    minor fix: update ruby style

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
-  autocomplete :course, :code_title_instructor, :full => true, :limit => 100, :extra_data => [:code, :title], :display_value => :full_title
+  autocomplete :course, :code_title_instructor, full: true, limit: 100, extra_data: [:code, :title], display_value: :full_title
 
   def index
     if params[:query].present?
