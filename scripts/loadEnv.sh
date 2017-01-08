560c62208b84e85d401b1aecd70b12b5cae9c717
Jonathan Cruz <jcruzdeveloper@gmail.com>
2017-01-08 23:56:00 +1100

    scripts: update loadEnv script
    * Make it executable
    * Minor terminal output changes
    * Run rake task for search autocomplete

--- scripts/loadEnv.sh
+++ scripts/loadEnv.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 clear
 
-echo "Setting up local environment for ClaremontReview\n"
+echo "Setting up local environment for ClaremontReview..."
 
-echo "Installing all needed gems"
+echo
+echo "Installing all needed gems..."
+echo
 bundle install
 
-echo "Starting up elasticsearch service"
+echo
+echo "Starting up elasticsearch service..."
+echo
 sudo service elasticsearch restart
 
-echo "Setting up local database"
+echo
+echo "Setting up local database..."
+echo
 bundle exec rake db:reset
 bundle exec rake db:migrate
 bundle exec rake task:importjson
 bundle exec rake task:slug
+bundle exec rake task:codetitleinstructor
 bundle exec rake searchkick:reindex CLASS=Course
